dc9d4d4e4b90fa0a685b91e91e982f36ef68da82
Torsten Anders <torsten.anders@beds.ac.uk>
2018-07-29 14:03:06 +0100

    Updated doc generation

--- generate-documentation.lisp
+++ generate-documentation.lisp
@@ -1,13 +1,15 @@
 ;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10 -*-
 
-;;; cldoc (https://gitlab.common-lisp.net/cldoc/cldoc/) -- documentation using 
+;;; Using cldoc (https://gitlab.common-lisp.net/cldoc/cldoc/) 
 ;;; Just works.  Minor doc string formatting supported.
 ;;; Perhaps only downside: internal and exported symbols are not distinguished. Workaround: mark auxiliary function names etc. with a leading underscore (e.g., _aux-function). 
 ;;; BUG: Nested functions (e.g.g, global functions within flet) not documented. Could this be fixed with cldoc::define-descriptor-handler?
 
 (require :cldoc)
 
-(cldoc:extract-documentation 'cludg:html "/Users/torsten/common-lisp/string-tools/doc/" 
+(defparameter *doc-dir* (make-pathname :directory (append (pathname-directory *load-truename*) '("doc"))))
+
+(cldoc:extract-documentation 'cludg:html (format nil "~A" *doc-dir*) 
     (asdf:find-system :string-tools)
     :table-of-contents-title 
     "String Tools")
